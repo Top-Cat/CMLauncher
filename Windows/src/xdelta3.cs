863a9d603c57db3f742a2c30626b6bde45e520a7
Thomas Cheyney <top_cat@thomasc.co.uk>
2026-04-22 11:11:51 +0100

    Add more margin for windows xdelta3 patches

--- Windows/src/xdelta3.cs
+++ Windows/src/xdelta3.cs
@@ -6,7 +6,7 @@ public class xdelta3
     /// <summary>
     /// Sets the maximum buffer size that xdelta3 is allowed to write to.
     /// </summary>
-    static readonly int MAX_BUFFER = 8 * 1024 * 1024; // 8 MB
+    static readonly int MAX_BUFFER = 32 * 1024 * 1024; // 32 MB
 
     /// <summary>
     /// Creates xdelta3 patch from source to target.
